c1054e08183e003a0d51b129e86f85fae618812b
Robert Bledsaw III <rbledsaw3@outlook.com>
2023-11-16 02:30:00 -0500

    added structs to avoid potential bugs due to easily swappable parameters.

--- DiceRoll.cpp
+++ DiceRoll.cpp
@@ -6,15 +6,25 @@
 #include <iostream>
 #include <random>
 
-int roll(int diceQty, int diceSides) {
+struct DiceQty {
+    int value;
+    explicit DiceQty(int qty) : value(qty) {}
+};
+
+struct DiceSides {
+    int value;
+    explicit DiceSides(int sides) : value(sides) {}
+};
+
+int roll(DiceQty diceQty, DiceSides diceSides) {
     std::random_device rDev; //initiates a random device
     
     //set the distribution bounds to the total number of die sides
-    std::uniform_int_distribution<int> dist(1, diceSides); 
+    std::uniform_int_distribution<int> dist(1, diceSides.value); 
 
     int results = 0;
     //sum the total number of die rolls
-    for(int i = 0; i < diceQty; ++i) {
+    for(int i = 0; i < diceQty.value; ++i) {
         results += dist(rDev);
     }
 
@@ -22,26 +32,17 @@ int roll(int diceQty, int diceSides) {
 }
 
 int main() {
-    std::cout << "Rolling 1d4: " << roll(1, 4) << "\n";
-    std::cout << "Rolling 2d4: " << roll(2, 4) << "\n";
-    std::cout << "Rolling 3d4: " << roll(3, 4) << "\n";
-    std::cout << "Rolling 4d4: " << roll(4, 4) << "\n";
-    std::cout << "Rolling 1d6: " << roll(1, 6) << "\n";
-    std::cout << "Rolling 2d6: " << roll(2, 6) << "\n";
-    std::cout << "Rolling 3d6: " << roll(3, 6) << "\n";
-    std::cout << "Rolling 4d6: " << roll(4, 6) << "\n";
-    std::cout << "Rolling 1d8: " << roll(1, 8) << "\n";
-    std::cout << "Rolling 2d8: " << roll(2, 8) << "\n";
-    std::cout << "Rolling 3d8: " << roll(3, 8) << "\n";
-    std::cout << "Rolling 4d8: " << roll(4, 8) << "\n";
-    std::cout << "Rolling 1d10: " << roll(1, 10) << "\n";
-    std::cout << "Rolling 2d10: " << roll(2, 10) << "\n";
-    std::cout << "Rolling 3d10: " << roll(3, 10) << "\n";
-    std::cout << "Rolling 4d10: " << roll(4, 10) << "\n";
-    std::cout << "Rolling 1d12: " << roll(1, 12) << "\n";
-    std::cout << "Rolling 2d12: " << roll(2, 12) << "\n";
-    std::cout << "Rolling 1d20: " << roll(1, 20) << "\n";
-    std::cout << "Rolling 1d100: " << roll(1, 100) << "\n";
-
+    
+    for (int i = 4; i <= 12; i += 2) {
+        for (int j = 1; j <= 4; ++j) {
+            std::cout << "Rolling " << j << "d" << i << ": " << roll(DiceQty(j), DiceSides(i)) << "\n";
+        }
+    }
+    for (int i = 20; i <= 100; i += 80) {
+        for (int j = 1; j <= 4; ++j) {
+            std::cout << "Rolling " << j << "d" << i << ": " << roll(DiceQty(j), DiceSides(i)) << "\n";
+        }
+    }
+    
     return 0;
 }
